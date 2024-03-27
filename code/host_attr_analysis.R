9e654897cb628c7f4d711abda0895543174f218a
Matan Markfeld <91337701+MatanMarkfeld@users.noreply.github.com>
2024-03-27 16:33:33 +0200

    host attributes

--- code/host_attr_analysis.R
+++ code/host_attr_analysis.R
@@ -10,7 +10,20 @@ rm(list=ls())
 
 # reading microbiome data
 data_asv <- read_csv("data/data_processed/microbiome/data_asv_rra0.01_th1000.csv") %>% 
-  filter(host_species == "Rattus rattus" & grid!="village")
+  filter(host_species == "Rattus rattus" & grid!="village" & village == "Mandena")
+
+# calculating ASV degree for each village
+asv_degree <- data_asv %>% 
+  count( asv_ID) %>% rename(asv_degree=n)
+data_asv %<>% left_join(asv_degree, by="asv_ID")
+# modules
+data_modules <- fun_modularity_analysis(data_asv)
+
+n_landuse <- data_modules %>% 
+  group_by(host_group) %>% 
+  summarise(n_grid = n_distinct(grid))
+
+host_grid <- data_modules %>% distinct(host_ID, host_group)
 
 # calculating ASV degree for each village
 asv_degree <- data_asv %>% 
@@ -18,18 +31,19 @@ asv_degree <- data_asv %>%
 
 # calculating the median ASV degree for each host
 host_median_degree <- data_asv %>% 
-  left_join(asv_degree, by=c("village","asv_ID")) %>% 
   group_by(host_ID) %>% 
-  summarise(median_degree = median(n), mean_degree = mean(n))
+  summarise(median_degree = median(asv_degree), mean_degree = mean(asv_degree))
 
 # adding host attributes
 host_attr <- read_csv("data/data_processed/small_mammals/small_mammals_attributes.csv")
 
 host_final_data <- host_median_degree %>% 
   left_join(host_attr, by="host_ID") %>% 
-  mutate(age_repro = factor(age_repro)) %>% 
-  mutate(pre = ifelse(median_degree<5,"low", ifelse(median_degree>27,"high", "medium"))) %>% 
-  mutate(pre = factor(pre, levels = c("low","medium","high"))) %>% 
+  left_join(host_grid, by="host_ID") %>% 
+  left_join(n_landuse, by="host_group") %>% 
+  #mutate(age_repro = factor(age_repro)) %>% 
+  mutate(pre = ifelse(n_grid<3,"low", ifelse(n_grid>4,"high", "medium"))) %>% 
+  mutate(pre = factor(pre, levels = c("low","medium","high"))) %>%   
   mutate(sex2 = ifelse(sex=="male",1,0)) %>% 
   mutate(age = ifelse(age_repro==1,1,0))
 
@@ -37,8 +51,14 @@ host_final_data <- host_median_degree %>%
 a=quantile(host_final_data$median_degree, c(0.1,0.5,0.9))
 
 # difference between groups
+diff_results <- host_final_data %>%
+  #filter(sex == "male") %>% 
+  group_by(pre) %>% 
+  summarise(n = mean(age_repro))
+count(age)
+
 host_final_data %>% 
-ggplot(aes(x=age_repro, y=median_degree, fill=age_repro)) + 
+ggplot(aes(x=pre, y=sex2)) + 
   geom_boxplot() + 
   theme_bw() +
   theme(axis.text = element_text(size = 14, color = 'black'), title = element_text(size = 20), legend.position = "none")
