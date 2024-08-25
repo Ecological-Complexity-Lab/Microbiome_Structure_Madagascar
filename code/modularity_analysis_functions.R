e1805ab6c9cfc77365b42b9b0de99b1047f63068
Matan Markfeld <91337701+MatanMarkfeld@users.noreply.github.com>
2024-08-25 16:38:42 +0300

    grid attributes

--- code/modularity_analysis_functions.R
+++ code/modularity_analysis_functions.R
@@ -192,6 +192,34 @@ fun_modules_similarity <- function(dat) {
   return(list(grid_modules_dist))
 }
 
+# function for calculating similarity in modules between grids-seasons
+fun_modules_similarity2 <- function(dat) {
+  
+  # how many host individuals in every grid
+  n_host_grid_season <- dat %>% 
+    distinct(host_ID, grid, season) %>% 
+    count(grid, season) %>% 
+    dplyr::rename(n_grid_season=n)
+  
+  # matrix of grid similarity in modules
+  grid_modules <- dat %>% 
+    group_by(grid, season, host_group) %>% 
+    summarise(host_n = n_distinct(host_ID)) %>% 
+    left_join(n_host_grid_season, by=c("grid","season")) %>% 
+    mutate(n = host_n/n_grid_season) %>% 
+    select(-host_n, -n_grid_season) %>% 
+    spread(host_group, n, fill = 0) %>% 
+    mutate(grid = as.character(grid)) %>%
+    arrange(grid, season) %>% 
+    unite("sample", grid:season, remove = TRUE) %>% 
+    column_to_rownames("sample") %>% 
+    as.matrix() 
+  
+  # calculating the *similarity* between grids
+  grid_modules_dist <- as.matrix(1-vegdist(grid_modules, method = "bray"))
+  
+  return(list(grid_modules_dist))
+}
 
 # function for plotting ASVs degree distribution
 fun_asv_degree_distribution <- function(dat) {
@@ -319,3 +347,112 @@ fun_calc_betaNTI <- function(dat_mat, phylo_dist, asv_pool, n_shuff) {
 
 ############################################################################
 
+
+fun_grid_attributes <- function(dat) {
+  
+  
+  habitat <- dat %>% 
+    mutate(grid_name = case_when(grid_name=="Primary forest" ~ "semi-intact_forest",
+                                 grid_name=="Degraded forest" ~ "semi-intact_forest",
+                                 grid_name=="Secondary forest" ~ "secondary_forest",
+                                 grid_name=="Savoka" ~ "brushy_regrowth",
+                                 grid_name=="Sugar cane" ~ "agriculture",
+                                 grid_name=="Mixed agricultural fields" ~ "agriculture",
+                                 grid_name=="Vanilla, coffee, banana fields" ~ "agriculture",
+                                 grid_name=="Rice field" ~ "flooded_rice",
+                                 grid_name=="Vanilla" ~ "agroforest",
+                                 grid_name=="Village" ~ "village")) %>% 
+    select(grid_name, season, plot, square, dead_logs, tree_dbh, tree_height, #omiting crown distance
+           liana_number, herbaceous_height, herbaceous_cover, canopy_cover) #omitting observations and season
+  ## deal with multitrunk trees by taking sqrt of sum of square DBH of each stem
+  habitat <- habitat %>% 
+    separate(tree_dbh, c("dbh1", "dbh2", "dbh3"), sep = ",") %>% #using unique(habitat$tree_dbh) see max of 3 stems, give each their own column
+    mutate(across(c(dbh1, dbh2, dbh3), as.numeric), #make numeric
+           across(c(dbh1, dbh2, dbh3), ~ifelse(is.na(.x), 0, .x)), #make na into 0
+           tree_dbh = dbh1^2+dbh2^2+dbh3^2, #sum sq dbhs
+           tree_dbh = ifelse(tree_dbh == 0, NA, sqrt(tree_dbh))) %>% #sqrt of sum, if 0 make NA (no trees)
+    select(-dbh1, -dbh2, -dbh3)
+  ## deal with the herbaceous_height "x to x"
+  habitat <- habitat %>% 
+    mutate(herbaceous_height = case_when(herbaceous_height == "0.4 to 1.5" ~ mean(c(0.4, 1.5)),
+                                         herbaceous_height == "1.5 to 2" ~ mean(c(1.5, 2)),
+                                         herbaceous_height == "0.3 to 1" ~ mean(c(0.3, 1)), 
+                                         herbaceous_height == "0,3 to 1" ~ mean(c(0.3, 1)), 
+                                         herbaceous_height == "0 to 1" ~ mean(c(0, 1)),
+                                         herbaceous_height == "0 to 2" ~ mean(c(0, 2)), 
+                                         TRUE ~ as.numeric(herbaceous_height)))
+  # add pitfall column
+  habitat <- habitat %>% 
+    mutate(pitfall = ifelse(str_detect(plot, "Pitfall"), "Pitfall", "Grid"),
+           pitfall_line = case_when(str_detect(plot, "Pitfall 1") ~ 1,
+                                    str_detect(plot, "Pitfall 2") ~ 2,
+                                    str_detect(plot, "Pitfall 3") ~ 3,
+                                    str_detect(plot, "Pitfall 4") ~ 4,
+                                    str_detect(plot, "Pitfall 5") ~ 5,
+                                    str_detect(plot, "Pitfall 6") ~ 6,
+                                    str_detect(plot, "Pitfall 7") ~ 7,
+                                    str_detect(plot, "Pitfall 8") ~ 8,
+                                    str_detect(plot, "Pitfall 9") ~ 9,
+                                    str_detect(plot, "Pitfall 10") ~ 10,
+                                    str_detect(plot, "Pitfall 11") ~ 11,
+                                    str_detect(plot, "Pitfall 12") ~ 12,
+                                    TRUE ~ 0))
+  
+  ## make a trees df
+  trees <- habitat %>% 
+    select(grid_name, season, pitfall, plot, square, tree_height, tree_dbh) %>% 
+    group_by(grid_name,season, plot, square) %>%
+    summarise(tree_height = mean(tree_height, na.rm=T),
+              tree_dbh = mean(tree_dbh, na.rm=T))
+  
+  ## create a new df individual tree measurements
+  squares <- habitat %>% 
+    mutate(tree = ifelse(is.na(tree_dbh), 0, 1)) %>% ## count the number of trees in each square
+    group_by(grid_name, season, plot, square) %>% 
+    mutate(n_trees = sum(tree)) %>% 
+    ungroup() %>% 
+    select(-tree_height, -tree_dbh, -tree) %>% # remove tree info from habitat
+    distinct() %>% 
+    left_join(trees, by=c("grid_name","season","plot","square"))
+  
+  # summarizing for plots
+  plot_sum <- squares %>% 
+    mutate(across(c(n_trees, tree_height, tree_dbh, dead_logs, 
+                    liana_number, herbaceous_height, 
+                    herbaceous_cover, canopy_cover))) %>% 
+    group_by(pitfall, pitfall_line, grid_name, season, plot) %>% 
+    summarise(n_trees = sum(n_trees, na.rm=T),
+              tree_height = mean(tree_height, na.rm=T),
+              tree_dbh = mean(tree_dbh, na.rm=T),
+              n_logs = sum(dead_logs, na.rm=T),
+              n_liana = sum(liana_number, na.rm=T),
+              m_herb_ht = mean(herbaceous_height, na.rm=T),
+              m_herb_cv = mean(herbaceous_cover, na.rm = T),
+              m_canopy_cv = mean(canopy_cover, na.rm = T)) %>% 
+    ungroup() %>% 
+    mutate(across(everything(), ~replace(. , is.nan(.), 0)))
+  
+  # summarizing for grids
+  grid_sum <- plot_sum %>% 
+    filter(pitfall == "Grid") %>% 
+    group_by(grid_name, season) %>% 
+    summarise_at(vars(4:11), ~mean(., na.rm=TRUE)) %>% 
+    ungroup() %>% 
+    arrange(grid_name) %>% 
+    mutate(grid_name = factor(grid_name))
+  
+  # transforming to matrix
+  grid_sum_mat <- grid_sum %>% 
+    unite(col="grid_season", grid_name:season, remove = TRUE) %>%
+    column_to_rownames("grid_season") %>%
+    ungroup() %>% 
+    as.matrix() %>% 
+    decostand(method = "range")
+  
+  #write_csv(grid_sum, "data/data_processed/village_attributes.csv")
+  
+  ## calculating *dis-similarity* between grids
+  distmat_grid <- as.matrix(vegdist(grid_sum_mat, method = "bray"))
+  
+  return(distmat_grid)
+}
