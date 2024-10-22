50cb136b34fb4aaeb69a84de9ed569d695b93347
Itamar Talpaz <talpazi@post.bgu.ac.il>
2024-10-22 14:05:49 +0300

    Update grids_attr_pca.R
    save asv_families matrix

--- code/grids_attr_pca.R
+++ code/grids_attr_pca.R
@@ -266,7 +266,7 @@ data_asv_family_mat <- data_asv_family %>%
   spread(Family, reads, fill = 0) %>% 
   column_to_rownames("host_ID") %>% 
   as.matrix()
-
+save(data_asv_family_mat,file="data_asv_family_mat.RData")
 # PCA
 pca_microbiome <- prcomp(data_asv_family_mat)
 
