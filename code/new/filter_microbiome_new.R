5435a434335551a4531edc577ff11c167dd86ea6
Matan Markfeld <91337701+MatanMarkfeld@users.noreply.github.com>
2025-01-25 10:08:29 +0200

    figure_S1

--- code/new/filter_microbiome_new.R
+++ code/new/filter_microbiome_new.R
@@ -122,15 +122,16 @@ for (i in seq(0.01,0.8, by=0.005)) {
 #pdf(file = 'results/figure_S1.pdf')
 
 asv_unique %>% 
-  ggplot(aes(x=i, y=asv_n)) + 
+  ggplot(aes(x=i, y=asv_n/1951)) + 
   geom_line() +
   geom_point() +
-  geom_vline(xintercept=c(0.2), linetype='dashed', color="gray") +
+  geom_vline(xintercept=0.2, linetype='dashed', color="gray") +
+  geom_hline(yintercept=103/1951, linetype='dashed', color="gray") +
   #scale_y_continuous(limits = c(0, 0.35)) +
   #scale_x_continuous(limits = c(0, 20)) +
   theme_bw() +
   theme(axis.text = element_text(size = 12, color = 'black'), title = element_text(size = 15), panel.grid = element_blank(), panel.border = element_rect(color = "black")) +
-  labs(x="ASV Prevalence", y="No. of ASVs")
+  labs(x="ASV Prevalence", y="Prop. of ASVs")
 #dev.off()
 
 # filtering the ASVs
@@ -237,71 +238,24 @@ length(unique(dat4$host_ID)) - length(unique(dat5$host_ID))
 # how many ASVs?
 length(unique(dat5$asv_ID)) # 1951 ASVs
 
+
+# plotting prevalence distribution
+n_host_final <- length(unique(dat5$host_ID))
+
+dat5 %>% 
+  count(asv_ID) %>% 
+  mutate(asv_p = n/n_host_final) %>% 
+  ggplot(aes(x=asv_p, y=..ncount..)) + 
+  geom_histogram() +
+  geom_vline(xintercept=c(0.2), linetype='dashed', color="gray") +
+  #scale_y_continuous(limits = c(0, 0.35)) +
+  #scale_x_continuous(limits = c(0, 20)) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 12, color = 'black'), title = element_text(size = 15), panel.grid = element_blank(), panel.border = element_rect(color = "black")) +
+  labs(x="ASV Prevalence", y="No. of ASVs")
+
+
 # saving the data
 write_csv(dat5, "data/data_processed/microbiome/data_asv_rra0.001_p0.02_th5000_all.csv")
 
 
-
-
-
-
-######################################## old
-### filtering out ASVs with less than threshold of relative abundance
-rel_reads_threshold <- 0.01
-dat_filtered_rel_abundance <- dat %>%
-  mutate(across(starts_with("ASV"),~ ./unfiltered_reads)) %>% 
-  mutate(across(starts_with("ASV"), ~ifelse(.<rel_reads_threshold,0,.)))
-
-# calculate updated total reads
-dat_filtered_rel_abundance %<>% mutate(across(starts_with("ASV"), ~.*unfiltered_reads)) %>% 
-  mutate(total_reads = select(.,starts_with("ASV")) %>% rowSums(na.rm = TRUE)) %>% 
-  mutate(across(starts_with("ASV"), ~./total_reads))
-
-# filtering out samples with less than threshold of reads
-reads_threshold <- 1000
-dat_filtered_threshold <- dat_filtered_rel_abundance %>% 
-  filter(total_reads >= reads_threshold)
-
-# transforming to long format
-data_asv_long_format <- dat_filtered_threshold %>% 
-  relocate(total_reads, .before = unfiltered_reads) %>% 
-  select(-unfiltered_reads) %>% 
-  gather("asv_ID", "reads", starts_with("ASV")) %>% 
-  filter(reads>0)
-
-#write_csv(data_asv_long_format, "data/data_processed/microbiome/data_asv_rra0.01_th1000.csv")
-
-
-#####
-# adding taxonomy
-tax <- read_delim("data/data_raw/data_microbiome/ASVs_taxonomy_new.tsv") %>% 
-  dplyr::rename(asv_ID = ASV)
-
-# setting the not allowed taxonomy:
-# not bacteria, chloroplast or mitochondria
-tax_exclude <- tax %>% 
-  filter(Kingdom != "Bacteria" | Order == "Chloroplast" | Family == "Mitochondria" | is.na(Kingdom))
-
-# finding phylogenetic distance
-# reading the phylogenetic tree
-best_tree <- readRDS(file = "results/phylo_tree_0.01_2.rds")
-phylo_tree <- best_tree$tree
-# ASVs phylogenetic distance
-asv_distance <- ape::cophenetic.phylo(phylo_tree)
-
-# mean distance for each ASV
-mean_phylo_dist <- rowMeans(asv_distance)
-hist(mean_phylo_dist)
-a <- names(mean_phylo_dist[mean_phylo_dist>3])
-b <- tax %>% filter(!(asv_ID %in% tax_exclude$asv_ID) & asv_ID %in% a)
-# Not many ASVs seem very different from all the rest (dist>4). Only ASV_4819 is not in one of the categories of the not allowed taxonomy. 
-# So I remove it from the analysis. 
-
-data_asv_long_format <- read_csv("data/data_processed/microbiome/data_asv_rra0.01_th1000.csv")
-
-data_asv_long_format_clean <- data_asv_long_format %>% 
-  filter(!(asv_ID %in% tax_exclude$asv_ID)) %>% 
-  filter(asv_ID != "ASV_4819")
-
-
-write_csv(data_asv_long_format_clean, "data/data_processed/microbiome/data_asv_rra0.01_th1000_clean.csv")
